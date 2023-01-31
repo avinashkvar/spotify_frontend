0bf1ded23fa12f5b89bc7cba230b428c8ee5bb31
avinashkvar <k3076183@gmail.com>
2023-01-31 15:09:30 +0530

    new commit

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,8 +1,17 @@
 import React from 'react'
 
 const Home = () => {
+  
+  const handleclick = ()=>{
+     fetch('https://spotify-avinashkvar.vercel.app/login')
+     .then((res)=>res.json()).then((res)=>console.log(res))
+     .catch((err)=>console.log(err))
+  }
   return (
-    <div>Home</div>
+    <div>Home
+        <button onClick={()=>handleclick()}>login</button>
+    </div>
+    
   )
 }
 
